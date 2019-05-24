b6828aa3b5f766d7a5d04f073c0731a49635a8b2
Cynthia Lu <lucynthi@gmail.com>
2019-05-24 12:46:46 -0400

    ADH_MDS Changes with positioning, no misalignment

--- practice_vsforce/name_replace_py.py
+++ practice_vsforce/name_replace_py.py
@@ -6,6 +6,6 @@ with open('ADH_MDS_copy.page', 'r') as in_file:
     for line in in_file:
         output.append(re.sub(r'rendered=\".*\"', 'rendered=\"true\"', re.sub(r'\>\{\!.*\}\<', '>Z<', line)))
 
-with open('ADH_MDS_replaceX.page.out', 'w') as out_file:
+with open('ADH_MDS_replaceX2.page.out', 'w') as out_file:
     for line in output:
         out_file.write(line)
